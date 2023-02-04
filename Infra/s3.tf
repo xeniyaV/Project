3a9a866581c2697a01136721139a76ba3854ad4b
xeniyaV <xvsunlife@gmail.com>
2023-02-04 00:39:27 -0600

    ga file

--- Infra/s3.tf
+++ Infra/s3.tf
@@ -1,13 +1,10 @@
 
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-
 # Creating s3 bucket and blocking public access
 resource "aws_s3_bucket" "bucket" {
   bucket                  = "efuse-bucket-work-sample"
   }
 
-# s3 bucket policy allowing public read access to objects within the bucket.
+# s3 bucket policy allowing public read access to objects within the bucket, list objects wont be allowed, since s3:ListObject is not part of the policy below.
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.bucket.bucket
 
