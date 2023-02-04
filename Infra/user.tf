e44de2122f8bee2f3ef1f5bbf1132aa10c7d9502
xeniyaV <xvsunlife@gmail.com>
2023-02-04 00:17:41 -0600

    ga file

---
+++ Infra/user.tf
@@ -0,0 +1,37 @@
+
+# Creating  IAM user
+resource "aws_iam_user" "iam_user" {
+  name = "efuse-s3-user"
+}
+
+
+# Creating Policy
+resource "aws_iam_policy" "user_s3policy" {
+  name = "efuse-s3-user-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+          "s3:ListBucket"
+        ],
+        Effect = "Allow",
+        Resource = [
+          "${aws_s3_bucket.bucket.arn}/*",
+           "${aws_s3_bucket.bucket.arn}"
+
+        ]
+      }
+    ]
+  })
+}
+
+# Attaching above policy to the IAM user
+resource "aws_iam_user_policy_attachment" "user_s3policy_attachment" {
+  user       = aws_iam_user.iam_user.name
+  policy_arn = aws_iam_policy.user_s3policy.arn
+}
